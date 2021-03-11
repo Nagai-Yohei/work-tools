ae4a7adf1268c7730d9b7a0760b30bff527359a5
Nagai-Yohei <nagai.yohei@gmail.com>
2021-03-11 23:43:54 +0900

    整列したタイル表示

--- src/app/calendar/calendar-days-events/calendar-days-events.component.ts
+++ src/app/calendar/calendar-days-events/calendar-days-events.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-calendar-days-events',
   templateUrl: './calendar-days-events.component.html',
   styleUrls: ['./calendar-days-events.component.css']
 })
 export class CalendarDaysEventsComponent implements OnInit {
+  tiles: Tile[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
+    this.tiles.push({text: 'One', cols:1, rows:1, color:'lightblue'});
   }
 
 }
